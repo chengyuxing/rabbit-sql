1c6e6af5a00581b0d5244d5479c39136ef2578bb
chengyuxing <chengyuxingo@gmail.com>
2022-10-21 23:17:07 +0800

    字符串常量模版改为区分大小写

--- src/main/java/com/github/chengyuxing/sql/BakiDao.java
+++ src/main/java/com/github/chengyuxing/sql/BakiDao.java
@@ -610,10 +610,10 @@ public class BakiDao extends JdbcSupport implements Baki {
             if (!constants.isEmpty()) {
                 for (String key : constants.keySet()) {
                     String constantName = "${" + key + "}";
-                    if (StringUtil.containsIgnoreCase(trimEndedSql, constantName)) {
+                    if (trimEndedSql.contains(constantName)) {
                         // use args first, if not exists then constants.
-                        if (!hasArgs || !CollectionUtil.containsKeyIgnoreCase(args, constantName)) {
-                            trimEndedSql = StringUtil.replaceIgnoreCase(trimEndedSql, constantName, constants.get(key));
+                        if (!hasArgs || !args.containsKey(constantName)) {
+                            trimEndedSql = trimEndedSql.replace(constantName, constants.get(key));
                         }
                     }
                 }

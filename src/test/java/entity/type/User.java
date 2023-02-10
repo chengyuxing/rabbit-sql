c8ddc7f14be49ca9641bb35e7beb4059d2bd466a
chengyuxing <chengyuxingo@gmail.com>
2023-02-10 20:26:44 +0800

    测试

---
+++ src/test/java/entity/type/User.java
@@ -0,0 +1,49 @@
+package entity.type;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Table(name = "user", schema = "test")
+public class User {
+    @Id
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private LocalDateTime now;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", now=" + now +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getNow() {
+        return now;
+    }
+
+    public void setNow(LocalDateTime now) {
+        this.now = now;
+    }
+}

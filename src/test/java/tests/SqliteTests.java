ad9f44c9b37502a3d0abaa0578aeb47ff8e984b8
chengyuxing <chengyuxingo@gmail.com>
2023-05-09 20:41:22 +0800

    增加decode函数关键字，以便识别高亮

--- src/test/java/tests/SqliteTests.java
+++ src/test/java/tests/SqliteTests.java
@@ -4,12 +4,15 @@ import com.github.chengyuxing.common.DataRow;
 import com.github.chengyuxing.sql.Baki;
 import com.github.chengyuxing.sql.BakiDao;
 import com.github.chengyuxing.sql.PagedResource;
+import com.github.chengyuxing.sql.page.PageHelper;
+import com.github.chengyuxing.sql.page.PageHelperProvider;
 import com.zaxxer.hikari.HikariDataSource;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
+import java.sql.DatabaseMetaData;
+import java.util.Collections;
+import java.util.Map;
 
 public class SqliteTests {
 
@@ -21,7 +24,27 @@ public class SqliteTests {
         dataSource = new HikariDataSource();
         dataSource.setDriverClassName("org.sqlite.JDBC");
         dataSource.setJdbcUrl("jdbc:sqlite:/Users/chengyuxing/Downloads/sqlite_data");
-        baki = BakiDao.of(dataSource);
+        BakiDao bakiDao = BakiDao.of(dataSource);
+        bakiDao.setPageHelperProvider(new PageHelperProvider() {
+            @Override
+            public PageHelper customPageHelper(DatabaseMetaData databaseMetaData, String dbName, char namedParamPrefix) {
+                if (dbName.equals("sqlite")) {
+                    return new PageHelper() {
+                        @Override
+                        public String pagedSql(String sql) {
+                            return sql + " limit 10 offset 0";
+                        }
+
+                        @Override
+                        public Map<String, Integer> pagedArgs() {
+                            return null;
+                        }
+                    };
+                }
+                return null;
+            }
+        });
+        baki = bakiDao;
     }
 
     @Test

9d562dc26cdedcb6b764b941c0c7dc39d47a62bb
chengyuxing <chengyuxingo@gmail.com>
2023-05-28 09:55:25 +0800

    insert增加插入实体方法

--- src/main/java/com/github/chengyuxing/sql/support/executor/InsertExecutor.java
+++ src/main/java/com/github/chengyuxing/sql/support/executor/InsertExecutor.java
@@ -1,7 +1,10 @@
 package com.github.chengyuxing.sql.support.executor;
 
+import com.github.chengyuxing.common.DataRow;
+
 import java.util.Collection;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 插入执行器
@@ -51,10 +54,30 @@ public abstract class InsertExecutor {
     public abstract int save(Map<String, ?> data);
 
     /**
+     * 插入保存一个实体
+     *
+     * @param entity 标准java bean实体
+     * @return 受影响的行数
+     */
+    public int saveEntity(Object entity) {
+        return save(DataRow.fromEntity(entity));
+    }
+
+    /**
      * 插入保存
      *
      * @param data 数据
      * @return 受影响的行数
      */
     public abstract int save(Collection<? extends Map<String, ?>> data);
+
+    /**
+     * 插入保存一组实体
+     *
+     * @param entities 一组标准java bean实体类
+     * @return 受影响的行数
+     */
+    public int saveEntities(Collection<Object> entities) {
+        return save(entities.stream().map(DataRow::fromEntity).collect(Collectors.toList()));
+    }
 }

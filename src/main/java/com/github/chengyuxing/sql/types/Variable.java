a303cc18e833ad0e203acb6d7d2924855740f6d3
chengyuxing <chengyuxingo@gmail.com>
2023-11-06 20:42:31 +0800

    Support custom Variable for parse sql template variable.

--- src/main/java/com/github/chengyuxing/sql/types/Variable.java
+++ src/main/java/com/github/chengyuxing/sql/types/Variable.java
@@ -2,13 +2,15 @@ package com.github.chengyuxing.sql.types;
 
 import com.github.chengyuxing.sql.utils.SqlUtil;
 
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * Variable interface for format object to custom string literal.
  *
- * @see ExamplePgArray
+ * @see ExampleDBArray
  */
 @FunctionalInterface
 public interface Variable {
@@ -23,21 +25,30 @@ public interface Variable {
      * Example handle postgresql array string literal.<br>
      * e.g. Object[]{"a", "b", "c"} {@code ->} {'a', 'b', 'c'}
      */
-    class ExamplePgArray implements Variable {
+    class ExampleDBArray implements Variable {
         private final Object[] array;
+        private final DatabaseMetaData metaData;
 
-        public ExamplePgArray(Object[] array) {
+        public ExampleDBArray(Object[] array, DatabaseMetaData metaData) {
             this.array = array;
+            this.metaData = metaData;
         }
 
         @Override
         public String stringLiteral() {
-            return Stream.of(array).map(item -> {
-                if (item instanceof String) {
-                    return SqlUtil.safeQuote((String) item);
+            try {
+                if (metaData.getDatabaseProductName().equalsIgnoreCase("postgresql")) {
+                    return Stream.of(array).map(item -> {
+                        if (item instanceof String) {
+                            return SqlUtil.safeQuote((String) item);
+                        }
+                        return item.toString();
+                    }).collect(Collectors.joining(", ", "{", "}"));
                 }
-                return item.toString();
-            }).collect(Collectors.joining(", ", "{", "}"));
+                return "other database implementation...";
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }

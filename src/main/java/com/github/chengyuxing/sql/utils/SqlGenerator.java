229b10d341ac742acdb086e047a3684dd2beee82
chengyuxing <chengyuxingo@gmail.com>
2024-06-23 22:23:13 +0800

    optimized.

--- src/main/java/com/github/chengyuxing/sql/utils/SqlGenerator.java
+++ src/main/java/com/github/chengyuxing/sql/utils/SqlGenerator.java
@@ -1,6 +1,6 @@
 package com.github.chengyuxing.sql.utils;
 
-import com.github.chengyuxing.common.script.Patterns;
+import com.github.chengyuxing.common.script.expression.Patterns;
 import com.github.chengyuxing.common.tuple.Pair;
 import com.github.chengyuxing.common.tuple.Tuples;
 import com.github.chengyuxing.common.utils.ObjectUtil;
--- src/main/java/com/github/chengyuxing/sql/utils/SqlHighlighter.java
+++ src/main/java/com/github/chengyuxing/sql/utils/SqlHighlighter.java
@@ -2,7 +2,7 @@ package com.github.chengyuxing.sql.utils;
 
 import com.github.chengyuxing.common.console.Color;
 import com.github.chengyuxing.common.console.Printer;
-import com.github.chengyuxing.common.script.Patterns;
+import com.github.chengyuxing.common.script.expression.Patterns;
 import com.github.chengyuxing.common.tuple.Pair;
 import com.github.chengyuxing.common.utils.StringUtil;
 import com.github.chengyuxing.sql.Keywords;

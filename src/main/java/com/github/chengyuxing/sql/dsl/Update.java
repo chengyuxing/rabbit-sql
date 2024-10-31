6bf473f04464c0b601d8bb4050551c9b0dfbe1d2
chengyuxing <chengyuxingo@gmail.com>
2024-10-31 19:37:11 +0800

    update optimized.

--- src/main/java/com/github/chengyuxing/sql/dsl/Update.java
+++ src/main/java/com/github/chengyuxing/sql/dsl/Update.java
@@ -1,7 +1,8 @@
 package com.github.chengyuxing.sql.dsl;
 
 import com.github.chengyuxing.sql.dsl.clause.Where;
-import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
 
 public abstract class Update<T> {
     protected boolean ignoreNull = false;
@@ -16,7 +17,7 @@ public abstract class Update<T> {
         return this;
     }
 
-    public abstract int saveById(@NotNull T entity);
+    public abstract int byId();
 
-    public abstract <NEW extends Where<T, NEW>> int save(@NotNull T entity, @NotNull NEW where);
+    public abstract int by(Function<Where<T>, Where<T>> where);
 }

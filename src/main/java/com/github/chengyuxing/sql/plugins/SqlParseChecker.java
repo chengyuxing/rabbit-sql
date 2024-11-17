45b3745638f84c4eb71ecce61d8400fa832e6f36
chengyuxing <chengyuxingo@gmail.com>
2024-11-17 18:15:17 +0800

    SqlParseChecker add param2 args

--- src/main/java/com/github/chengyuxing/sql/plugins/SqlParseChecker.java
+++ src/main/java/com/github/chengyuxing/sql/plugins/SqlParseChecker.java
@@ -2,13 +2,16 @@ package com.github.chengyuxing.sql.plugins;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
+
 @FunctionalInterface
 public interface SqlParseChecker {
     /**
      * Do something for sql.
      *
-     * @param sql sql
+     * @param sql  sql
+     * @param args args
      * @return new sql
      */
-    @NotNull String handle(@NotNull final String sql);
+    @NotNull String handle(@NotNull final String sql, @NotNull Map<String, Object> args);
 }

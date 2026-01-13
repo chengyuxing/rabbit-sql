95ae3e27e7c6b854d2c0db733bbc34e5c62a0b5e
chengyuxing <chengyuxingo@gmail.com>
2026-01-13 10:22:43 +0800

    code optimized.

--- src/main/java/com/github/chengyuxing/sql/exceptions/DynamicSqlParseException.java
+++ src/main/java/com/github/chengyuxing/sql/exceptions/DynamicSqlParseException.java
@@ -1,10 +1,6 @@
 package com.github.chengyuxing.sql.exceptions;
 
 public class DynamicSqlParseException extends RuntimeException {
-    public DynamicSqlParseException(String message) {
-        super(message);
-    }
-
     public DynamicSqlParseException(String message, Throwable cause) {
         super(message, cause);
     }

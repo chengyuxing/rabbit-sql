ee06e5aa96d981d1ea6eb044f9b3bfdd91b75eb9
chengyuxing <chengyuxingo@gmail.com>
2026-01-17 11:54:23 +0800

    code and performance optimized.

--- src/main/java/com/github/chengyuxing/sql/yaml/HyphenatedPropertyUtil.java
+++ src/main/java/com/github/chengyuxing/sql/yaml/HyphenatedPropertyUtil.java
@@ -1,6 +1,6 @@
 package com.github.chengyuxing.sql.yaml;
 
-import com.github.chengyuxing.common.util.StringUtils;
+import com.github.chengyuxing.common.util.NamingUtils;
 import org.yaml.snakeyaml.introspector.Property;
 import org.yaml.snakeyaml.introspector.PropertyUtils;
 
@@ -11,6 +11,6 @@ public class HyphenatedPropertyUtil extends PropertyUtils {
     @Override
     public Property getProperty(Class<?> type, String name) {
         setSkipMissingProperties(true);
-        return super.getProperty(type, StringUtils.camelize(name));
+        return super.getProperty(type, NamingUtils.kebabToCamel(name));
     }
 }
